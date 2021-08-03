634c58a821aaa56f3479492d827c1f811db9190a
mw_dev <mingwai148@gmail.com>
2021-08-03 17:07:03 +0800

    Adds register page

---
+++ oganik-html/register.php
@@ -0,0 +1,278 @@
+<?php
+  // Include config file
+  require_once "config.php";
+ 
+  // Define variables and initialize with empty values
+  $username = $password = $confirm_password = "";
+  $username_err = $password_err = $confirm_password_err = "";
+ 
+  // Processing form data when form is submitted
+  if ($_SERVER["REQUEST_METHOD"] == "POST") { // $_SERVER["REQUEST_METHOD"] Returns the request method used to access the page (such as POST)
+ 
+    // Validate username
+    if (empty(trim($_POST["username"]))) {
+
+        $username_err = "Please enter a username.";
+
+    } elseif (!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))) {
+
+        $username_err = "Username can only contain letters, numbers, and underscores.";
+
+    } else {
+        // Prepare a select statement
+
+        $sql = "SELECT id FROM user WHERE username = '" . trim($_POST["username"]) . "'";
+        $result = mysqli_query($link, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+            $username_err = "Username is taken";
+        } else {
+            $username = trim($_POST["username"]);
+        }
+    }
+    
+    // Validate password
+    if (empty($_POST["password"])) {
+        $password_err = "Please enter a password.";    
+
+    } elseif (strlen(trim($_POST["password"])) < 6) {
+        $password_err = "Password must have atleast 6 characters.";
+
+    } else {
+        $password = $_POST["password"];
+
+    }
+    
+    // Validate confirm password
+    if (empty($_POST["confirm_password"])) {
+        $confirm_password_err = "Please confirm password.";     
+
+    } else {
+        $confirm_password = $_POST["confirm_password"];
+
+        if (empty($password_err) && ($password != $confirm_password)) {
+            $confirm_password_err = "Password did not match.";
+
+        }
+    }
+    
+    // Check input errors before inserting in database
+    if (empty($username_err) && empty($password_err) && empty($confirm_password_err)) {
+        
+        // Prepare an insert statement
+        $sql = "INSERT INTO user (username, password) VALUES ('$username', '$password')";
+         
+        
+        if (mysqli_query($link, $sql)) {
+          echo "New record created successfully";
+          header("location: login.php");
+
+        } else {
+          echo "Error: " . $sql . "<br>" . mysqli_error($link);
+
+        }
+    }
+    // Close connection
+    mysqli_close($link);
+  }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Home || TheGrabGroceries</title>
+    <!-- favicons Icons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicons/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicons/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicons/favicon-16x16.png" />
+    <link rel="manifest" href="assets/images/favicons/site.webmanifest" />
+    <meta name="description" content="Agrikon HTML Template For Agriculture Farm & Farmers" />
+
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Homemade+Apple&family=Abril+Fatface&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+
+
+    <link rel="stylesheet" href="assets/vendors/bootstrap/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/vendors/bootstrap-select/bootstrap-select.min.css" />
+    <link rel="stylesheet" href="assets/vendors/animate/animate.min.css" />
+    <link rel="stylesheet" href="assets/vendors/fontawesome/css/all.min.css" />
+    <link rel="stylesheet" href="assets/vendors/jarallax/jarallax.css" />
+    <link rel="stylesheet" href="assets/vendors/organik-icon/organik-icons.css" />
+    <link rel="stylesheet" href="assets/vendors/jquery-magnific-popup/jquery.magnific-popup.css" />
+    <link rel="stylesheet" href="assets/vendors/nouislider/nouislider.min.css" />
+    <link rel="stylesheet" href="assets/vendors/nouislider/nouislider.pips.css" />
+    <link rel="stylesheet" href="assets/vendors/odometer/odometer.min.css" />
+    <link rel="stylesheet" href="assets/vendors/swiper/swiper.min.css" />
+    <link rel="stylesheet" href="assets/vendors/tiny-slider/tiny-slider.min.css" />
+    <link rel="stylesheet" type="assets/css" href="css/organik.css">
+
+    <!-- template styles -->
+    <link rel="stylesheet" href="assets/css/organik.css" />
+    <style>
+        body { 
+          font: 14px sans-serif; 
+          background-image: url("https://cdn.wallpapersafari.com/93/26/Stkyof.gif")
+        }
+        .signup-form{ width: 360px; padding: 20px; }
+    </style>
+</head>
+
+<body>
+    <div class="preloader">
+        <img class="preloader__image" width="55" src="assets/images/loader.png" alt="" />
+    </div>
+    <!-- /.preloader -->
+    <div class="page-wrapper">
+        <header class="main-header">
+            <div class="topbar">
+                <div class="container">
+                    <div class="main-logo">
+                        <a href="index.php" class="logo">
+                            <img src="assets/images/logo-dark.png" width="105" alt="">
+                        </a>
+                        <div class="mobile-nav__buttons">
+                            <a href="#" class="search-toggler"><i class="organik-icon-magnifying-glass"></i></a>
+                            <a href="#" class="mini-cart__toggler"><i class="organik-icon-shopping-cart"></i></a>
+                        </div><!-- /.mobile__buttons -->
+
+                        <span class="fa fa-bars mobile-nav__toggler"></span>
+                    </div><!-- /.main-logo -->
+
+                    <div class="topbar__left">
+                        <div class="topbar__social">
+                            <a href="#" class="fab fa-twitter"></a>
+                            <a href="#" class="fab fa-facebook-square"></a>
+                            <a href="#" class="fab fa-instagram"></a>
+                        </div><!-- /.topbar__social -->
+                        <div class="topbar__info">
+                            <i class="organik-icon-email"></i>
+                            <p>Email <a href="mailto:info@organik.com">info@organik.com</a></p>
+                        </div><!-- /.topbar__info -->
+                    </div><!-- /.topbar__left -->
+                    <div class="topbar__right">
+                        <div class="topbar__info">
+                            <i class="organik-icon-calling"></i>
+                            <p>Phone <a href="tel:+92-666-888-0000">92 666 888 0000</a></p>
+                        </div><!-- /.topbar__info -->
+                        <div class="topbar__buttons">
+                            <a href="#" class="search-toggler"><i class="organik-icon-magnifying-glass"></i></a>
+                            <a href="#" class="mini-cart__toggler"><i class="organik-icon-shopping-cart"></i></a>
+                        </div><!-- /.topbar__buttons -->
+                    </div><!-- /.topbar__left -->
+
+                </div><!-- /.container -->
+            </div><!-- /.topbar -->
+            <nav class="main-menu">
+                <div class="container">
+                    <div class="main-menu__login">
+                        <a href="#"><i class="organik-icon-user"></i>Login / Register</a>
+                    </div><!-- /.main-menu__login -->
+                    <ul class="main-menu__list">
+                        <li class="dropdown">
+                            <a href="index.php">Home</a>
+                            <ul>
+                                <li>
+                                    <a href="index.php">Home One</a>
+                                </li>
+                                <li><a href="index-2.php">Home Two</a></li>
+                                <li class="dropdown">
+                                    <a href="#">Header Styles</a>
+                                    <ul>
+                                        <li><a href="index.php">Header One</a></li>
+                                        <li><a href="index-2.php">Header Two</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="about.php">About</a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="products.php">Shop</a>
+                            <ul>
+                                <li><a href="products.php">Shop</a></li>
+                                <li><a href="product-details.php">Product Details</a></li>
+                                <li><a href="cart.php">Cart Page</a></li>
+                                <li><a href="checkout.php">Checkout</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown"><a href="news.php">News</a>
+                            <ul>
+                                <li><a href="news.php">News</a></li>
+                                <li><a href="news-details.php">News Details</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                    <div class="main-menu__language">
+                        <img src="assets/images/resources/flag-1-1.jpg" alt="">
+                        <label class="sr-only" for="language-select">select language</label>
+                        <!-- /#language-select.sr-only -->
+                        <select class="selectpicker" id="language-select-header">
+                            <option value="english">English</option>
+                            <option value="arabic">Arabic</option>
+                        </select>
+                    </div><!-- /.main-menu__language -->
+                </div><!-- /.container -->
+            </nav>
+            <div class="signup-form container loginbox">
+                <h2>Sign Up</h2>
+                <p>Please fill this form to create an account.</p>
+                <form 
+                action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); /* $_SERVER["PHP_SELF"] Returns the filename of the currently executing script */ ?>" 
+                method="post">
+                    <div class="form-group">
+                        <label>Username</label> </br>
+                        <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                        <span class="invalid-feedback"><?php echo $username_err; ?></span>
+                    </div>    
+                    <div class="form-group">
+                        <label>Password</label> </br>
+                        <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
+                        <span class="invalid-feedback"><?php echo $password_err; ?></span>
+                    </div>
+                    <div class="form-group">
+                        <label>Confirm Password</label> </br>
+                        <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
+                        <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                        <input type="reset" class="btn btn-secondary ml-2" value="Reset">
+                    </div>
+                    <p>Already have an account? <a href="login.php">Login here</a>.</p>
+                </form>
+            </div>    
+
+    <!-- /.search-popup -->
+
+    <a href="#" data-target="html" class="scroll-to-target scroll-to-top"><i class="fa fa-angle-up"></i></a>
+
+
+    <script src="assets/vendors/jquery/jquery-3.5.1.min.js"></script>
+    <script src="assets/vendors/bootstrap/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendors/bootstrap-select/bootstrap-select.min.js"></script>
+    <script src="assets/vendors/jarallax/jarallax.min.js"></script>
+    <script src="assets/vendors/jquery-ajaxchimp/jquery.ajaxchimp.min.js"></script>
+    <script src="assets/vendors/jquery-appear/jquery.appear.min.js"></script>
+    <script src="assets/vendors/jquery-circle-progress/jquery.circle-progress.min.js"></script>
+    <script src="assets/vendors/jquery-magnific-popup/jquery.magnific-popup.min.js"></script>
+    <script src="assets/vendors/jquery-validate/jquery.validate.min.js"></script>
+    <script src="assets/vendors/nouislider/nouislider.min.js"></script>
+    <script src="assets/vendors/odometer/odometer.min.js"></script>
+    <script src="assets/vendors/swiper/swiper.min.js"></script>
+    <script src="assets/vendors/tiny-slider/tiny-slider.min.js"></script>
+    <script src="assets/vendors/wnumb/wNumb.min.js"></script>
+    <script src="assets/vendors/wow/wow.js"></script>
+    <script src="assets/vendors/isotope/isotope.js"></script>
+    <script src="assets/vendors/countdown/countdown.min.js"></script>
+    <!-- template js -->
+    <script src="assets/js/organik.js"></script>
+</body>
+
+</html>

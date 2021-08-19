b6cde2087a02caa5548d9f73dd99c94ef8c17819
mw_dev <mingwai148@gmail.com>
2021-08-19 17:31:05 +0800

    Restricts accessing profile if not logged in

--- oganik-html/login.php
+++ oganik-html/login.php
@@ -4,8 +4,8 @@ session_start();
  
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    //header("location: index.php");
-    //exit;
+    header("location: index.php");
+    exit;
 }
  
 // Include config file

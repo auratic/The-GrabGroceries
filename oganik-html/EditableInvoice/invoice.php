355f00cc6549468c3d9abb77c83c5981e8ce5168
Jie <1191200921@student.mmu.edu.my>
2021-11-11 15:33:18 +0800

    change title

--- oganik-html/EditableInvoice/invoice.php
+++ oganik-html/EditableInvoice/invoice.php
@@ -37,7 +37,7 @@ if (isset($_GET["id"])) {
 <head>
 	<meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
 
-	<title>Editable Invoice</title>
+	<title>Invoice</title>
 
 	<link rel="apple-touch-icon" sizes="180x180" href="../assets/images/favicons/apple-touch-icon.png" />
     <link rel="icon" type="image/png" sizes="32x32" href="../assets/images/favicons/favicon-32x32.png" />
